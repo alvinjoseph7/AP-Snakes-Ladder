c26972c44054f8d2e29db910ff38cc77b2f17956
Pratyush Jain <Pratyushjain12@gmail.com>
2021-12-19 13:02:51 +0530

    Commit_2

--- AP_Ladder/src/main/java/com/example/ap_ladder/HelloController.java
+++ AP_Ladder/src/main/java/com/example/ap_ladder/HelloController.java
@@ -2,8 +2,11 @@ package com.example.ap_ladder;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
+    @FXML
+    private ImageView img_1;
     @FXML
     private Label welcomeText;
 
