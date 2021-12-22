9b4fcb99aca647e85187998e533a062da56cf931
alvinjoseph7 <alvin9joseph@gmail.com>
2021-12-23 03:19:10 +0530

    change stage title

--- AP_Ladder/src/main/java/com/example/ap_ladder/HelloApplication.java
+++ AP_Ladder/src/main/java/com/example/ap_ladder/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400 , 550);
-        stage.setTitle("Hello!");
+        stage.setTitle("Ludo Master");
         stage.setScene(scene);
         stage.show();
     }
